b24b705e09517013db8a91c9bffac750c4cffc3d
faishal H <84917658+make-something@users.noreply.github.com>
2024-07-18 16:10:25 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/JeepQuest2/';
+$config['base_url'] = 'https://make-something.github.io/JeepQuest/';
 
 /*
 |--------------------------------------------------------------------------
